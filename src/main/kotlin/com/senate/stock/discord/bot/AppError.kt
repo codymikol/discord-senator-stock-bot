60d9b057c7c46c27d63cd8312970b5a6f8f6ae23
johnflynn <johnflynn493@gmail.com>
2021-01-07 23:27:39 -0500

    formatting.

--- src/main/kotlin/com/senate/stock/discord/bot/AppError.kt
+++ src/main/kotlin/com/senate/stock/discord/bot/AppError.kt
@@ -4,6 +4,6 @@ sealed class AppError(val message: String) {
 
     class NoReportForDate(date: String): AppError("No Report found for date $date")
 
-    class InvalidDateFormat(date: String, userId: Long): AppError("Sorry <@!$userId> I couldn't parse the date you provided $date make sure it is in the format YYYY-MM-DD")
+    class InvalidDateFormat(date: String, userId: Long): AppError("Sorry <@!$userId> I couldn't parse the date you provided '$date' make sure it is in the format YYYY-MM-DD")
 
 }
