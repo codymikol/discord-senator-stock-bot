308f5b6733035b6574c5d19d174ce2c976060df1
johnflynn <johnflynn493@gmail.com>
2021-01-10 13:23:29 -0500

    Updated ci workflow to print generated test report.

--- src/test/kotlin/com/senate/stock/discord/bot/BotApplicationTests.kt
+++ src/test/kotlin/com/senate/stock/discord/bot/BotApplicationTests.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.web.context.WebApplicationContext
 
-@SpringBootTest
 class BotApplicationTests {
 
 	@Autowired
